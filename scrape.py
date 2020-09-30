a0438a76ec1c2e813655453c3904340bf33b724a
kentotamura <root@tiancunjianrens-MacBook-Air.local>
2020-09-30 17:59:46 +0800

    scrape emoji

--- scrape.py
+++ scrape.py
@@ -3,6 +3,7 @@ import requests
 from bs4 import BeautifulSoup
 import re
 import os
+import csv
 
 emoji_list = []
 word_list = []
@@ -10,7 +11,11 @@ point_list = []
 
 site_url = "https://www.emojiall.com/zh-hant/all-emojis"
 get_url = requests.get(site_url)
+# print (get_url.text)
 soup = BeautifulSoup(get_url.text, "lxml")
+
+for x in soup.find_all("a",{"class":"emoji_font tooltip"}):
+    x.extract()
 
 for script in soup(["span"]):
     script.extract()
@@ -18,20 +23,34 @@ for script in soup(["span"]):
 emojis = soup.select('.emoji_font')
 words = soup.select('.emoji_name')
 
-for emoji in emojis:
-    emoji = emoji.getText().replace('\u200d','')
-    if not repr(emoji).startswith(r"'\U") and len(emoji) < 2:
-        emoji_list.append(emoji)
-    else:
-        continue
-# print(emoji_list)
-
-for emoji in emoji_list:
-    code_point = hex(ord(emoji))
-    point_list.append(code_point)
-# print(point_list)
-
 for word in words:
     word = word.getText()
     word_list.append(word)
-print(word_list)
+# print(len(word_list))
+
+for emoji in emojis:
+    emoji = emoji.getText()
+    emoji_list.append(emoji)
+# print(len(emoji_list))
+
+for emoji in emoji_list:
+    if len(emoji) < 2:
+        code_point = hex(ord(emoji))
+        point_list.append(code_point)
+# print(point_list)
+
+dic = dict(zip(emoji_list,word_list))
+# print(dic)
+
+new_list = []
+for k,v in list(dic.items()):
+    if len(k) > 2:
+        dic.pop(k)
+    else:
+        new_list.append((k,v))
+# print(new_list)
+
+file0 = "/Users/ktamura/Downloads/emoji.txt"
+with open(file0, 'w') as f:
+    for _unicode, (emoji, word) in zip(point_list, list(new_list)):
+        f.writelines('{},{},{}\n'.format(_unicode, emoji, word))
